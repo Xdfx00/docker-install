ba6cc6e91ff46a4667015882ec2357b84db8ef0c
Shaikh Aman <147152214+Xdfx00@users.noreply.github.com>
2025-09-04 15:43:18 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -50,7 +50,7 @@ centos_almalinux_rockylinux_rhel() {
   sudo dnf -y install dnf-plugins-core
   sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/$ID/docker-ce.repo
 
-  sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
   sudo systemctl enable --now docker
 }
 
